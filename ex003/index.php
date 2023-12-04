8ba6a66e7a63895a7cb54a715c790c537b21791f
Filipe Santos <filipejs.santos21@gmail.com>
2023-12-03 23:59:49 -0300

    conhecendo alguns tipos em php

---
+++ ex003/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos em PHP</title>
+</head>
+<body>
+    <h1>Alguns tipos em PHP:</h1>
+    <?php 
+        $name = "Filipe Santos";
+        $age = 21;
+        $weight = 70.0;
+        $married = true;
+
+        echo "<p>Olá $name!</p>";
+        echo "<p>É muito bom saber que você já está com $age anos.</p>";
+        echo "<p>O seu peso está $weight</p>";
+        echo "<p>E o seu estado de casado é: $married</p>";
+    ?>
+</body>
+</html>
