b4f6593451e16a54aa17b876ba4ce12a758fbe38
Filipe Santos <filipejs.santos21@gmail.com>
2023-12-03 23:59:19 -0300

    adicionando titulo a pagina

--- ex002/index.php
+++ ex002/index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Data e Hora em PHP</title>
     </head>
     <body>
         <h1>Exibindo data e hora:</h1>
